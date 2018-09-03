33acb178ffadc8aad008509289bc98621001a7c6
Matthew Poling <polingm2@dewv.edu>
2018-09-03 19:16:21 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -11,15 +11,15 @@ class PlayerWizard extends Sprite {
         this.height = 48;
         this.x = 400;
         this.y = 552;
-        this.speedWhenWalking = 100;
+        this.speedWhenWalking = 200;
     }
     handleDownArrowKey() {
         this.speed = this.speedWhenWalking;
         this.angle = 270;
     }
     handleUpArrowKey() {
-        this.speed = this.speedWhenWalking;
-        this.angle = 90;
+        spell.x = this.x;
+        spell.y = this.y;
     }
     handleLeftArrowKey() {
         this.speed = this.speedWhenWalking;
@@ -29,6 +29,57 @@ class PlayerWizard extends Sprite {
         this.speed = this.speedWhenWalking;
         this.angle = 0;
     }
+    handleGameLoop() { //keeps Marcus in display area
+        this.x = Math.max(0, this.x);
+        this.x = Math.min(752, this.x);
+        this.speed = 0;
+    }
 }
 let marcus = new PlayerWizard();
 marcus.name = "marcus";
+
+class Spell extends Sprite {
+    constructor() {
+        super();
+        this.speed = 400;
+        this.height = 48;
+        this.width = 48;
+    }
+    handleBoundryContact() {
+        game.removeSprite(this);
+    }
+}
+
+let spell = new Spell();
+spell.name = ("A spell cast by Marcus");
+spell.angle = 90;
+spell.setImage("baseball.png");
+
+class NonPlayerWizard extends Sprite {
+    constructor() {
+        super();
+        this.name = ("The mysterious stranger");
+        this.setImage("bird.png");
+        this.width = 48;
+        this.height = 48;
+        this.x = 400;
+        this.y = this.height;
+        this.angle = 180;
+        this.speed = 150;
+    }
+    handleGameLoop() {
+        if(this.x <= 0) {
+            //leftward motion has reached left corner, so turn right
+            this.x = 0;
+            this.angle = 0;
+        }
+        if(this.x >= 750) {
+            //Rightward motion has reached right corner, so turn left
+            this.x = 750;
+            this.angle = 180;
+        }
+    }
+}
+
+let stranger = new NonPlayerWizard();
+stranger.name = ("stranger");
--- wizardsloader.js
+++ wizardsloader.js
@@ -2,3 +2,5 @@ import {game} from "./sgc/sgc.js";
 
 game.preloadImage("Stadium.png");
 game.preloadImage("andrewMccutchen.png");
+game.preloadImage("baseball.png");
+game.preloadImage("bird.png");
